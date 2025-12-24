3ce5359ac3c03cadf4a5932b5d2d4ea20cac6e23
liuliu61 <102744706+liuliu61@users.noreply.github.com>
2025-12-24 16:29:22 +0800

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,128 @@
+﻿//var wzck_host = 'https://api.123weizhushou.com';//正式
+// var wzck_host = 'http://char.gaorenyazhi.com';//测试
+ //var wzck_host = 'http://192.168.3.178:8080';//本地
+//var wzck_dl_host = 'https://sso.123weizhushou.com';
+
+var source = '推广';
+
+var isBd_ocpx = true;
+
+//套餐列表
+var wzck_kbbk_ktfw_list = [
+    {
+		id: 300005,
+		name: '蓝V认证',
+		time: '终身',
+		price: 399,
+		oldprice: 399*2
+	},
+    {
+		id: 300006,
+		name: '蓝v认证 + 线索留资',
+		time: '终身',
+		price: 599,
+		oldprice: 599*2
+	},
+    {
+		id: 300007,
+		name: '蓝v认证 + 商城',
+		time: '终身',
+		price: 999,
+		oldprice: 999*2
+	},
+    {
+		id: 300008,
+		name: '蓝v认证 + 团购',
+		time: '终身',
+		price: 1299,
+		oldprice: 1299*2
+	},
+    {
+		id: 300009,
+		name: '蓝v认证 + 线索留资 + 商城 + 团购',
+		time: '终身',
+		price: 1699,
+		oldprice: 1699*2
+	},
+    // {
+	// 	id: 300000,
+	// 	name: '抖店开通+搭建服务',
+	// 	time: '终身',
+	// 	price: 296,
+	// 	oldprice: 296*2
+	// },
+	// {
+	// 	id: 300001,
+	// 	name: '抖店开通+永久蓝v+搭建服务',
+	// 	time: '终身',
+	// 	price: 596,
+	// 	oldprice: 596*2
+	// }, {
+	// 	id: 300002,
+	// 	name: '抖店开通+永久蓝v+门店入驻+开通团购+搭建服务',
+	// 	time: '终身',
+	// 	price: 796,
+	// 	oldprice: 796*2
+	// }, {
+	// 	id: 300003,
+	// 	name: '抖店开通+永久蓝v+门店入驻+开通团购+会员营销系统+搭建服务',
+	// 	time: '终身',
+	// 	price: 1296,
+	// 	oldprice: 1296*2
+	// }, {
+	// 	id: 300004,
+	// 	name: '抖店开通+永久蓝v+门店入驻+开通团购+会员营销系统+分销带货+搭建服务',
+	// 	time: '终身',
+	// 	price: 1696,
+	// 	oldprice: 1696*2
+	// }
+];
+
+// // 网页文本注入
+$.ajax({
+	url: wzck_host + "/open/api/get_icp",
+	type: "post",
+	data: {
+		domain:window.location.host
+	},
+	success: function(r) {
+		if(r.data) {
+			$('.footer').html('<img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/gab.png" style="width: 18px;margin-bottom: -4px;" /><img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/icp.png" style="width: 18px;margin: 0 10px;margin-bottom: -4px;" />Copyright©'+new Date().getFullYear()+" <a href='http://beian.miit.gov.cn' style='color:#929799;'>"+(r.data.icpNum || '') + '</a>' + r.data.companyName + '&版权所有 盗版必究');
+			$('.wzck_footer_bot').html('<img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/gab.png" style="width: 18px;margin-bottom: -4px;" /><img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/icp.png" style="width: 18px;margin: 0 10px;margin-bottom: -4px;" />Copyright©'+new Date().getFullYear()+" <a href='http://beian.miit.gov.cn' style='color:#929799;'>"+(r.data.icpNum || '')+'</a> &版权所有 盗版必究 '+r.data.companyName);
+			$(".app_xieyi_name").html(r.data.companyName);
+	        $(".app_xieyi_names").html(r.data.companyName);
+	        localStorage.setItem("platform", r.data.platform);
+		} else {
+			$('.footer').html('<img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/gab.png" style="width: 18px;margin-bottom: -4px;" /><img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/icp.png" style="width: 18px;margin: 0 10px;margin-bottom: -4px;" />Copyright©'+new Date().getFullYear()+'<a href="http://beian.miit.gov.cn"></a> &版权所有 盗版必究 ');
+			$('.wzck_footer_bot').html('<img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/gab.png" style="width: 18px;margin-bottom: -4px;" /><img src="https://wzck-1252188577.cos.ap-beijing.myqcloud.com/www/icp.png" style="width: 18px;margin: 0 10px;margin-bottom: -4px;" />Copyright©'+new Date().getFullYear()+'<a href="http://beian.miit.gov.cn"></a> &版权所有 盗版必究 ');
+	    }
+	}
+});
+
+function goTop() {
+	$("html,body").animate({
+		"scrollTop": 0
+	}, 300)
+}
+
+$(document).ready(function() { //在文档加载完毕后执行
+	$(window).scroll(function() { //开始监听滚动条
+		scrollfun()
+	})
+})
+
+scrollfun();
+function scrollfun(){
+	//获取当前滚动条高度
+	var topp = $(document).scrollTop();
+	//用于调试 弹出当前滚动条高度
+	//alert(topp);
+	//判断如果滚动条大于90则弹出 "ok"
+	if(topp > 600) {
+		$('.biaoti').css('bottom', '0px');
+		$('.wzck_lbbkefu_box_bot').css('display', 'block');
+	} else {
+		$('.biaoti').css('bottom', '-100px');
+		$('.wzck_lbbkefu_box_bot').css('display', 'none');
+	}
+}
---
+++ static/js/ktfw.js
@@ -0,0 +1,296 @@
+﻿//订单套餐id
+var wzck_kbbk_ktfw_id = "";
+var orderNoStatusNum = 60;
+//默认选择的套餐
+zxktfw_b(0);
+var dytgShowModal = false; // 弹窗
+var isdytgShowModal = false; // 弹窗
+setTimeout(function () {
+	if (!isdytgShowModal) {
+		isdytgShowModal = true;
+		$(".dytg_showModal_bg").css("display", "flex");
+		setTimeout(function () {
+			if ($(".wzck_kbbk_ktbg").css("display") == "none" && $(".dytg_showModal_bg").css("display") == "flex") {
+				$(".dytg_showModal_bg").css("display", "none");
+				$(".wzck_kbbk_ktbg").css("display", "flex");
+				// 确认服务
+				$("#wzck_kbbk_qrxx").css("display", "block");
+				// 实时开通
+				$("#wzck_kbbk_sskt").css("display", "none");
+				//回显手机号
+				if (localStorage.getItem("TgDytgData")) {
+					var orderData = JSON.parse(localStorage.getItem("TgDytgData"));
+					$("#wzck_kbbk_phone2").val(orderData.applyMobile);
+				}
+                let phone = $("#banner_phone").val();
+                let regexp1 = /^1\d{10}$/;
+                if (phone && regexp1.test(phone)) {
+                    $("#wzck_kbbk_phone2").val(phone);
+                }
+				let subject = $(this).attr("data-subject");
+				if (subject) {
+					$(".sqbb_box_a").removeClass("qrxx_nav_boxac");
+					$(".sqbb_box_a[data-subject=" + subject + "]").addClass("qrxx_nav_boxac");
+				}
+			}
+		}, 5000);
+	}
+}, 5000);
+// 打开开通服务
+$(".ktfw").click(function () {
+	isdytgShowModal = true;
+	$(".dytg_showModal_bg").css("display", "none");
+	$(".wzck_kbbk_ktbg").css("display", "flex");
+	// 确认服务
+	$("#wzck_kbbk_qrxx").css("display", "block");
+	// 实时开通
+	$("#wzck_kbbk_sskt").css("display", "none");
+	//回显手机号
+	if (localStorage.getItem("TgDytgData")) {
+		var orderData = JSON.parse(localStorage.getItem("TgDytgData"));
+		$("#wzck_kbbk_phone2").val(orderData.applyMobile);
+	}
+    let phone = $("#banner_phone").val();
+    let regexp1 = /^1\d{10}$/;
+    if (phone && regexp1.test(phone)) {
+		$("#wzck_kbbk_phone2").val(phone);
+	}
+	let subject = $(this).attr("data-subject");
+	if (subject) {
+		$(".sqbb_box_a").removeClass("qrxx_nav_boxac");
+		$(".sqbb_box_a[data-subject=" + subject + "]").addClass("qrxx_nav_boxac");
+	}
+});
+
+// 关闭开通服务
+$(".wzck_kbbk_sskt_cuo").click(function () {
+	$(".wzck_kbbk_ktbg").css("display", "none");
+	orderNoStatusNum = 0;
+});
+// 关闭推荐服务
+$(".wzck_kbbk_dytg_cuo").click(function () {
+	$(".dytg_showModal_bg").css("display", "none");
+});
+
+// 客户开通记录
+var swiper1 = new Vue({
+	el: "#swiper2",
+	data: {
+		list: swiper1_list,
+	},
+});
+// 客户开通记录
+var swiper1 = new Vue({
+	el: "#swiper1",
+	data: {
+		list: swiper1_list,
+	},
+});
+// 案例
+var wzck_tmdzy = new Vue({
+	el: "#wzck_tmdzy",
+	data: {
+		list: wzckApp_tmdzy_anli_list,
+	},
+});
+// 案例
+var wzck_tmdzy1 = new Vue({
+	el: "#wzck_tmdzy1",
+	data: {
+		list: wzckApp_tmdzy_anli_list1,
+	},
+});
+
+// 选择申请主体
+var subject = "个体户";
+$(".sqbb_box_a").click(function () {
+	subject = $(this).attr("data-subject");
+	$(".sqbb_box_a").removeClass("qrxx_nav_boxac");
+	$(".sqbb_box_a[data-subject=" + subject + "]").addClass("qrxx_nav_boxac");
+});
+
+// 开通服务
+$(".sqbb_box_b").click(function () {
+	let index = $(this).attr("data-index");
+	zxktfw_b(index);
+});
+// console.log(wzck_kbbk_ktfw_list)
+// 选择套餐
+function zxktfw_b(index) {
+	$(".sqbb_box_b").removeClass("qrxx2_nav_boxac");
+	$(".sqbb_box_b[data-index=" + index + "]").addClass("qrxx2_nav_boxac");
+	wzck_kbbk_ktfw_id = wzck_kbbk_ktfw_list[index].id;
+	$(".ktbg_box_right_qrxx_money").html(wzck_kbbk_ktfw_list[index].price);
+	$(".ktbg_box_right_qrxx_moneyOld").html("¥" + wzck_kbbk_ktfw_list[index].oldprice);
+}
+
+// 勾选服务协议
+$("#wzck_kbbk_checkbox").click(function () {
+	if ($("#wzck_kbbk_checkbox").attr("data-i") == "1") {
+		$("#wzck_kbbk_checkbox").attr("data-i", "0");
+	} else {
+		$("#wzck_kbbk_checkbox").attr("data-i", "1");
+	}
+});
+
+// 立即开通下单
+$("#wzck_kbbk_ljsq2").click(function () {
+	if ($("#wzck_kbbk_checkbox").attr("data-i") == "0") {
+		alert("请先阅读服务协议");
+		return;
+	}
+
+	var phone = $("#wzck_kbbk_phone2").val();
+
+	if (!phone) {
+		alert("请输入手机号!");
+		return;
+	}
+
+	var regexp1 = /^1\d{10}$/;
+	if (!regexp1.test(phone)) {
+		alert("请输入正确的手机号！");
+		return;
+	}
+
+	$("#alipay").css("display", "none");
+	$("#wxpay").css("display", "none");
+
+	payFrom();
+});
+
+function payFrom() {
+	var phone = $("#wzck_kbbk_phone2").val();
+	$.ajax({
+		type: "post",
+		async: false,
+		data: {
+			productId: wzck_kbbk_ktfw_id,
+			applyName: "",
+			applyMobile: phone,
+			host: location.host,
+			source: source,
+			subject: subject,
+			keyword: localStorage.getItem("q"),
+			platform: localStorage.getItem("platform"),
+			pageUrl: localStorage.getItem("bdoCPX"),
+		},
+		url: wzck_host + "/open/api/wzck/douyin_order/submit",
+		success: function (res) {
+			if (res.code == 0) {
+				localStorage.setItem("TgDytgData", JSON.stringify(res.data));
+				$("#wzck_kbbk_qrxx").css("display", "none");
+				$("#wzck_kbbk_sskt").css("display", "block");
+				$("#wzck_kbbk_ktcg").css("display", "none");
+
+				if (res.data.payType == "wxpay" || res.data.payType == "weixin") {
+					$("#wxpay").css("display", "block");
+				}
+
+				if (res.data.payType == "alipay") {
+					$("#alipay").css("display", "block");
+				}
+
+				$(".wzck_kbbk_money").html(res.data.money);
+				$(".ktbg_box_right_sskt_code img").attr("src", res.data.qrcode);
+				orderNoStatusNum = 60;
+				$(".ktbg_box_right_sskt_code_bg").css("display", "none");
+				orderNoStatus();
+			} else if (res.code == 10002) {
+				alert("您已是会员！");
+				$(".wzck_kbbk_ktbg").css("display", "none");
+				$("#wzck_kbbk_qrxx").css("display", "none");
+				$("#wzck_kbbk_sskt").css("display", "none");
+				$("#wzck_kbbk_ktcg").css("display", "none");
+				// $('.dlbg').css('display', 'flex');
+			} else {
+				alert(res.msg);
+			}
+		},
+	});
+}
+
+//查询订单状态
+var serviceCode = "";
+var wzck_kbbk_ktcg_num = 4; //3秒后跳转系统
+function orderNoStatus() {
+	orderNoStatusNum--;
+	if (orderNoStatusNum < 1) {
+		$(".ktbg_box_right_sskt_code_bg").css("display", "flex");
+		return;
+	}
+	var orderData = JSON.parse(localStorage.getItem("TgDytgData"));
+	setTimeout(function () {
+		$.ajax({
+			url: wzck_host + "/open/api/wzck/douyin_order/get_order_status",
+			type: "post",
+			data: {
+				orderNo: orderData.orderNo,
+			},
+			success: function (r) {
+				if (r.code == 0) {
+					if (r.data.status == "paid") {
+						serviceCode = r.data.serviceCode;
+						$("#wzck_kbbk_sskt").css("display", "none");
+						$("#wzck_kbbk_ktcg").css("display", "block");
+						wzck_kbbk_ktcg_num = 4;
+						//3秒后跳转系统
+						gotoXt();
+					} else {
+						orderNoStatus();
+					}
+				} else {
+					orderNoStatus();
+				}
+			},
+		});
+	}, 3000);
+}
+
+//我已付款
+$(".ktbg_box_right_sskt_code_btn1").click(function () {
+	var orderData = JSON.parse(localStorage.getItem("TgDytgData"));
+	$.ajax({
+		url: wzck_host + "/open/api/wzck/douyin_order/get_order_status",
+		type: "post",
+		data: {
+			orderNo: orderData.orderNo,
+		},
+		success: function (r) {
+			if (r.code == 0) {
+				if (r.data.status == "paid") {
+					serviceCode = r.data.serviceCode;
+					$("#wzck_kbbk_sskt").css("display", "none");
+					$("#wzck_kbbk_ktcg").css("display", "block");
+					wzck_kbbk_ktcg_num = 4;
+					//3秒后跳转系统
+					gotoXt();
+				} else {
+					alert("该订单未支付，如已支付请联系客服！");
+				}
+			} else {
+				alert("查询订单状态失败");
+			}
+		},
+	});
+});
+
+//重新扫码
+$(".ktbg_box_right_sskt_code_btn2").click(function () {
+	$(".ktbg_box_right_sskt_code_bg").css("display", "none");
+	orderNoStatusNum = 60;
+	orderNoStatus();
+});
+
+//跳转系统
+function gotoXt() {
+	wzck_kbbk_ktcg_num = wzck_kbbk_ktcg_num - 1;
+	$("#wzck_kbbk_ktcg_num").text(wzck_kbbk_ktcg_num);
+	if (wzck_kbbk_ktcg_num <= 1) {
+		location.href = "http://www.wfwzs.com/dylvjfy/index.html?serviceCode=" + serviceCode;
+		return;
+	}
+	setTimeout(function () {
+		gotoXt();
+	}, 1000);
+}
